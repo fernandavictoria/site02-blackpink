66addcf47fba2a017bf387882d3b7aea2bbece0e
fernandavictoria <fernandamayfield@gmail.com>
2023-08-22 16:46:31 -0300

    socorro

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,13 +1,16 @@
-
+import * as S from "./style"
 import {Outlet} from "react-router-dom"
-import { ComponentHeader, ComponentFooter } from "../../components";
+import { ComponentHeader, ComponentFooter } from "../../components"
+import React from "react"
 
 
 export function Layout() {
     return (
       <>
         <ComponentHeader />
+        <S.Main>
           <Outlet />
+          </S.Main>
       <ComponentFooter />
     </>
   )
